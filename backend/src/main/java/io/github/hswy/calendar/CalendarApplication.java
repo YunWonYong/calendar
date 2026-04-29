3f6afc900d11f8f8aceb7b8cb2256ba844efef62
wonyong.yun <ywyi1992@gmail.com>
2026-04-30 01:04:33 +0900

    HelloController 추가.

--- backend/src/main/java/io/github/hswy/calendar/CalendarApplication.java
+++ backend/src/main/java/io/github/hswy/calendar/CalendarApplication.java
@@ -3,7 +3,7 @@ package io.github.hswy.calendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.github.hswy.calendar")
 public class CalendarApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalendarApplication.class, args);
