f5303d6df8a457b5640576f9a07d08bbac12bd17
wonyong.yun <ywyi1992@gmail.com>
2026-06-24 16:58:28 +0900

    [backend] access token properties 추가 및 기존 파일 수정.

--- backend/src/test/java/io/github/hswy/calendar/global/properties/frontend/FrontendPropertiesTest.java
+++ backend/src/test/java/io/github/hswy/calendar/global/properties/frontend/FrontendPropertiesTest.java
@@ -1,15 +1,27 @@
 package io.github.hswy.calendar.global.properties.frontend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest(
+    classes = FrontendProperties.class
+)
+@EnableConfigurationProperties
 public abstract class FrontendPropertiesTest {
     @Autowired
     private FrontendProperties frontendProperties;
 
+    @Test
+    public void TestInitProperties() {
+        assertThat(frontendProperties).isNotNull();
+        System.out.println(frontendProperties);
+    }
+    
     protected abstract void baseUrlTest();
     protected void baseUrlTest(String baseUrl) {
         assertThat(frontendProperties).isNotNull();
@@ -21,5 +33,20 @@ public abstract class FrontendPropertiesTest {
         assertThat(frontendProperties).isNotNull();
         assertThat(frontendProperties.getOauth2SuccessUrl()).isEqualTo(successUrl);
         assertThat(frontendProperties.getOauth2FailureUrl()).isEqualTo(failureUrl);
+    }
+
+    protected abstract void accessTokenTest();
+    protected void accessTokenTest(boolean skipFlag) {
+        if (skipFlag) {
+            return;
+        }
+        assertThat(frontendProperties).isNotNull();
+
+        AccessTokenProperties accessTokenProperties = frontendProperties.getAccessToken();
+        assertThat(accessTokenProperties).isNotNull();
+
+        assertThat(accessTokenProperties.getJwtSecret()).isNotEmpty();
+        assertThat(accessTokenProperties.getJwtExpiredSeconds()).isNotZero();
+        assertThat(accessTokenProperties.getRefreshExpiredSeconds()).isNotZero();
     }
 }
