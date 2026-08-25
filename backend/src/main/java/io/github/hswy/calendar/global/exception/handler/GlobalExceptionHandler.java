c1a2c191e7f56720b479ad69bb1f6453d7e4c1cd
wonyong.yun <ywyi1992@gmail.com>
2026-08-25 17:46:06 +0900

    [backend] GlobalExceptionHandler 추가.

---
+++ backend/src/main/java/io/github/hswy/calendar/global/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package io.github.hswy.calendar.global.exception.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import io.github.hswy.calendar.global.common.model.ApiResponseBody;
+import io.github.hswy.calendar.global.exception.model.ApplicationException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<ApiResponseBody<Void>> handleApplicationException(ApplicationException e, HttpServletRequest request) {
+        log.error(
+            "GlobalExceptionHandler type: {}, method: {}, url: {}, errorCode: {}, errorMessage: {}", 
+            "custom",
+            request.getMethod(),
+            request.getRequestURI(),
+            e.getCode(),
+            e.getMessage(),
+            e
+        );
+        
+        return ResponseEntity
+            .status(e.getStatus())
+            .body(ApiResponseBody.fail(e.getCode()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseBody<Void>> handleException(Exception e, HttpServletRequest request) {
+        log.error(
+            "GlobalExceptionHandler type: {}, method: {}, url: {}, errorMessage: {}", 
+            "unexpected",
+            request.getMethod(),
+            request.getRequestURI(),
+            e.getMessage(),
+            e
+        );
+
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ApiResponseBody.fail("INTERNAL_SERVER_ERROR"));
+    }
+}
