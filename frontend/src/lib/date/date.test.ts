165a5a1b4db61837f686958075d05a1abc32f9bf
wonyong.yun <ywyi1992@gmail.com>
2026-09-02 17:55:55 +0900

    [frontend] date 유틸 기능 및 테스트 코드 추가.

---
+++ frontend/src/lib/date/date.test.ts
@@ -0,0 +1,385 @@
+import { getCurrentMonth, getDateList, getNextMonth, getPrevMonth } from "./date";
+
+describe("getCurrentMonth 함수 테스트.", () => {
+    test("case 1: 2026.09", () => {
+        const year = 2026;
+        const month = 8;
+        const currentDate = getCurrentMonth(year, month);
+        expect(currentDate)
+            .toMatchObject({
+                year,
+                month: 8,
+                lastDate: 30,
+                firstDayIndex: 2,
+                lastDayIndex: 3,
+            });
+    });
+    test("case 2: 2026.02", () => {
+        const year = 2026;
+        const month = 1;
+        const currentDate = getCurrentMonth(year, month);
+        expect(currentDate)
+            .toMatchObject({
+                year: 2026,
+                month: 1,
+                lastDate: 28,
+                firstDayIndex: 0,
+                lastDayIndex: 6,
+            });
+    });
+    test("case 3: 2026.10", () => {
+        const year = 2026;
+        const month = 9;
+        const currentDate = getCurrentMonth(year, month);
+        expect(currentDate)
+            .toMatchObject({
+                year: 2026,
+                month: 9,
+                lastDate: 31,
+                firstDayIndex: 4,
+                lastDayIndex: 6,
+            });
+    });
+    test("case 4: Leap year", () => {
+        const year = 2024;
+        const month = 1;
+        const currentDate = getCurrentMonth(year, month);
+        expect(currentDate)
+            .toMatchObject({
+                year: 2024,
+                month: 1,
+                lastDate: 29,
+                firstDayIndex: 4,
+                lastDayIndex: 4,
+            });
+    });
+});
+
+describe("getPrevMonth 함수 테스트.", () => {
+    test("case 1: normal", () => {
+        const year = 2026;
+        const month = 8;
+        const prevDate = getPrevMonth(year, month);
+        expect(prevDate)
+            .toMatchObject({
+                year,
+                month: 7,
+                lastDate: 31,
+                firstDayIndex: 6,
+                lastDayIndex: 1,
+            });
+    });
+    test("case 2: overflow year", () => {
+        const year = 2026;
+        const month = 0;
+        const prevDate = getPrevMonth(year, month);
+        expect(prevDate)
+            .toMatchObject({
+                year: 2025,
+                month: 11,
+                lastDate: 31,
+                firstDayIndex: 1,
+                lastDayIndex: 3,
+            });
+    });
+    test("case 3: Leap year", () => {
+        const year = 2024;
+        const month = 2;
+        const prevDate = getPrevMonth(year, month);
+        expect(prevDate)
+            .toMatchObject({
+                year: 2024,
+                month: 1,
+                lastDate: 29,
+                firstDayIndex: 4,
+                lastDayIndex: 4,
+            });
+    });
+});
+
+describe("getNextDate 함수 테스트.", () => {
+    test("case 1: normal", () => {
+        const year = 2026;
+        const month = 8;
+        const prevDate = getNextMonth(year, month);
+        expect(prevDate)
+            .toMatchObject({
+                year,
+                month: 9,
+                lastDate: 31,
+                firstDayIndex: 4,
+                lastDayIndex: 6,
+            });
+    });
+    test("case 2: overflow year", () => {
+        const year = 2026;
+        const month = 11;
+        const prevDate = getNextMonth(year, month);
+        expect(prevDate)
+            .toMatchObject({
+                year: 2027,
+                month: 0,
+                lastDate: 31,
+                firstDayIndex: 5,
+                lastDayIndex: 0,
+            });
+    });
+    test("case 3: Leap year", () => {
+        const year = 2024;
+        const month = 0;
+        const prevDate = getNextMonth(year, month);
+        expect(prevDate)
+            .toMatchObject({
+                year: 2024,
+                month: 1,
+                lastDate: 29,
+                firstDayIndex: 4,
+                lastDayIndex: 4,
+            });
+    });
+});
+
+describe("getDateList 함수 테스트.", () => {
+    test("case 1: 2026.09", () => {
+        const year = 2026;
+        const month = 8;
+        const dateList = getDateList(year, month);
+        expect(Array.isArray(dateList)).toBeTruthy();
+        expect(dateList.length).toBe(35);
+        expect(dateList[0])
+            .toMatchObject({
+                year: 2026,
+                month: 7,
+                date: 30,
+                dayIndex: 0,
+                isCurrentMonthDate: false,
+                weekNo: 1,
+            });
+        expect(dateList[1])
+            .toMatchObject({
+                year: 2026,
+                month: 7,
+                date: 31,
+                dayIndex: 1,
+                isCurrentMonthDate: false,
+                weekNo: 1,
+            });
+        expect(dateList[2])
+            .toMatchObject({
+                year: 2026,
+                month: 8,
+                date: 1,
+                dayIndex: 2,
+                isCurrentMonthDate: true,
+                weekNo: 1,
+            });
+        expect(dateList[31])
+            .toMatchObject({
+                year: 2026,
+                month: 8,
+                date: 30,
+                dayIndex: 3,
+                isCurrentMonthDate: true,
+                weekNo: 5,
+            });
+        expect(dateList[dateList.length - 1])
+            .toMatchObject({
+                year: 2026,
+                month: 9,
+                date: 3,
+                dayIndex: 6,
+                isCurrentMonthDate: false,
+                weekNo: 5,
+            });
+    });
+    test("case 2: 2026.12", () => {
+        const year = 2026;
+        const month = 11;
+        const dateList = getDateList(year, month);
+        expect(Array.isArray(dateList)).toBeTruthy();
+        expect(dateList.length).toBe(35);
+        expect(dateList[0])
+            .toMatchObject({
+                year: 2026,
+                month: 10,
+                date: 29,
+                dayIndex: 0,
+                isCurrentMonthDate: false,
+                weekNo: 1,
+            });
+        expect(dateList[1])
+            .toMatchObject({
+                year: 2026,
+                month: 10,
+                date: 30,
+                dayIndex: 1,
+                isCurrentMonthDate: false,
+                weekNo: 1,
+            });
+        expect(dateList[2])
+            .toMatchObject({
+                year: 2026,
+                month: 11,
+                date: 1,
+                dayIndex: 2,
+                isCurrentMonthDate: true,
+                weekNo: 1,
+            });
+        expect(dateList[32])
+            .toMatchObject({
+                year: 2026,
+                month: 11,
+                date: 31,
+                dayIndex: 4,
+                isCurrentMonthDate: true,
+                weekNo: 5,
+            });
+        expect(dateList[dateList.length - 1])
+            .toMatchObject({
+                year: 2027,
+                month: 0,
+                date: 2,
+                dayIndex: 6,
+                isCurrentMonthDate: false,
+                weekNo: 5,
+            });
+    });
+    test("case 3: 2024.02 Leap year", () => {
+        const year = 2024;
+        const month = 1;
+        const dateList = getDateList(year, month);
+        expect(Array.isArray(dateList)).toBeTruthy();
+        expect(dateList.length).toBe(35);
+        expect(dateList[0])
+            .toMatchObject({
+                year: 2024,
+                month: 0,
+                date: 28,
+                dayIndex: 0,
+                isCurrentMonthDate: false,
+                weekNo: 1,
+            });
+        expect(dateList[3])
+            .toMatchObject({
+                year: 2024,
+                month: 0,
+                date: 31,
+                dayIndex: 3,
+                isCurrentMonthDate: false,
+                weekNo: 1,
+            });
+        expect(dateList[4])
+            .toMatchObject({
+                year: 2024,
+                month: 1,
+                date: 1,
+                dayIndex: 4,
+                isCurrentMonthDate: true,
+                weekNo: 1,
+            });
+        expect(dateList[32])
+            .toMatchObject({
+                year: 2024,
+                month: 1,
+                date: 29,
+                dayIndex: 4,
+                isCurrentMonthDate: true,
+                weekNo: 5,
+            });
+        expect(dateList[dateList.length - 1])
+            .toMatchObject({
+                year: 2024,
+                month: 2,
+                date: 2,
+                dayIndex: 6,
+                isCurrentMonthDate: false,
+                weekNo: 5,
+            });
+    });
+    test("case 4: 2027.01 size 42", () => {
+        const year = 2027;
+        const month = 0;
+        const dateList = getDateList(year, month);
+        expect(Array.isArray(dateList)).toBeTruthy();
+        expect(dateList.length).toBe(42);
+        expect(dateList[0])
+            .toMatchObject({
+                year: 2026,
+                month: 11,
+                date: 27,
+                dayIndex: 0,
+                isCurrentMonthDate: false,
+                weekNo: 1,
+            });
+        expect(dateList[4])
+            .toMatchObject({
+                year: 2026,
+                month: 11,
+                date: 31,
+                dayIndex: 4,
+                isCurrentMonthDate: false,
+                weekNo: 1,
+            });
+        expect(dateList[5])
+            .toMatchObject({
+                year: 2027,
+                month: 0,
+                date: 1,
+                dayIndex: 5,
+                isCurrentMonthDate: true,
+                weekNo: 1,
+            });
+        expect(dateList[35])
+            .toMatchObject({
+                year: 2027,
+                month: 0,
+                date: 31,
+                dayIndex: 0,
+                isCurrentMonthDate: true,
+                weekNo: 6,
+            });
+        expect(dateList[dateList.length - 1])
+            .toMatchObject({
+                year: 2027,
+                month: 1,
+                date: 6,
+                dayIndex: 6,
+                isCurrentMonthDate: false,
+                weekNo: 6,
+            });
+    });
+    test("case 5: 2026.02 size 28", () => {
+        const year = 2026;
+        const month = 1;
+        const dateList = getDateList(year, month);
+        expect(Array.isArray(dateList)).toBeTruthy();
+        expect(dateList.length).toBe(28);
+        expect(dateList[0])
+            .toMatchObject({
+                year: 2026,
+                month: 1,
+                date: 1,
+                dayIndex: 0,
+                isCurrentMonthDate: true,
+                weekNo: 1,
+            });
+        expect(dateList[1])
+            .toMatchObject({
+                year: 2026,
+                month: 1,
+                date: 2,
+                dayIndex: 1,
+                isCurrentMonthDate: true,
+                weekNo: 1,
+            });
+        expect(dateList[dateList.length - 1])
+            .toMatchObject({
+                year: 2026,
+                month: 1,
+                date: 28,
+                dayIndex: 6,
+                isCurrentMonthDate: true,
+                weekNo: 4,
+            });
+    });
+});
---
+++ frontend/src/lib/date/date.ts
@@ -0,0 +1,108 @@
+// 년도와 월에 따라 반환되는 배열의 사이즈가 유동적임. size: 28, 35, 42
+export const getDateList = (year: number, month: number) => {
+    const dateList = [];
+    const currentDate = getCurrentMonth(year, month);
+    const currentFirstDayIndex = currentDate.firstDayIndex;
+    let weekNo = 1;
+    if (currentFirstDayIndex > 0) {
+        const prevDate = getPrevMonth(
+            currentDate.year,
+            currentDate.month,
+        );
+        let i = currentFirstDayIndex - 1;
+        let dayIndex = 0;
+        while(i > -1) {
+            dateList.push({
+                year: prevDate.year,
+                month: prevDate.month,
+                date: prevDate.lastDate - i,
+                dayIndex: dayIndex,
+                isCurrentMonthDate: false,
+                weekNo,
+            });
+            --i;
+            ++dayIndex;
+        }
+    }
+
+    let lastDate = currentDate.lastDate
+    let date = 1;
+    let dayIndex = currentFirstDayIndex;
+    while(date < lastDate + 1) {
+        dateList.push({
+            year: currentDate.year,
+            month: currentDate.month,
+            date,
+            dayIndex: dayIndex,
+            isCurrentMonthDate: true,
+            weekNo,
+        });
+        ++date;
+        ++dayIndex;
+        if (dayIndex === 7) {
+            dayIndex = 0;
+            ++weekNo;
+        }
+    }
+
+    if (currentDate.lastDayIndex < 6) {
+        const nextMonth = getNextMonth(
+            currentDate.year,
+            currentDate.month,
+        );
+        const { lastDayIndex } = currentDate;
+        let i = 1;
+        do {
+            dateList.push({
+                year: nextMonth.year,
+                month: nextMonth.month,
+                date: i,
+                dayIndex: lastDayIndex + i,
+                isCurrentMonthDate: false,
+                weekNo,
+            });
+            ++i;
+        } while((i + lastDayIndex) < 7);
+    }
+    return dateList;
+};
+
+export const getCurrentMonth = (year: number, month: number) => {
+    const d = newUTCDate(year, month, 1);
+    const d2 = newUTCDate(year, month + 1, 0);
+    return {
+        year,
+        month,
+        lastDate: d2.getUTCDate(),
+        firstDayIndex: d.getUTCDay(),
+        lastDayIndex: d2.getUTCDay(),
+    };
+};
+
+export const getPrevMonth = (year: number, month: number) => {
+    const d = newUTCDate(year, month, 0);
+    const d2 = newUTCDate(year, month - 1, 1);
+    return { 
+        year: d.getUTCFullYear(), 
+        month: d.getUTCMonth(), 
+        lastDate: d.getUTCDate(),
+        firstDayIndex: d2.getUTCDay(), 
+        lastDayIndex: d.getUTCDay(), 
+    };
+};
+
+export const getNextMonth = (year: number, month: number) => {
+    const d = newUTCDate(year, month + 1, 1);
+    const d2 = newUTCDate(year, month + 2, 0);
+    return { 
+        year: d.getUTCFullYear(), 
+        month: d.getUTCMonth(), 
+        lastDate: d2.getUTCDate(),
+        firstDayIndex: d.getUTCDay(), 
+        lastDayIndex: d2.getUTCDay(), 
+    };
+};
+
+const newUTCDate = (year: number, month: number, date: number = 1) => {
+    return new Date(Date.UTC(year, month, date));
+};
