bc2063c43f620caabdac919a7a5dbe2391aa0ce9
wonyong.yun <ywyi1992@gmail.com>
2026-09-09 17:19:46 +0900

    [frontend] custom Link 태그에서 clickEventName props 추가.

--- frontend/src/components/link.tsx
+++ frontend/src/components/link.tsx
@@ -10,22 +10,25 @@ type LinkProps = {
         hash?: string;
     } | string;
     className?: string;
+    clickEventName?: string;
     children: ReactNode;
 };
 
-const Link = ({ to, className, children }: LinkProps) => {
+const Link = ({ to, className, children, clickEventName }: LinkProps) => {
     const location = useLocation();
     if (typeof to === "string") {
         to = { pathname: to };
+    }
+
+    if (!clickEventName) {
+        clickEventName = "PAGE_MOVE";
     }
     return (
         <L 
             to={ to }
             className={ className }
             onClick={(() => {
-                trackClick(
-                    `PAGE_MOVE@${location.pathname}=>${to.pathname}`,
-                )
+                trackClick(`${clickEventName}@${location.pathname}=>${to.pathname}`)
             })}
         >
             {
