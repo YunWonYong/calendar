a0085c900bc86f35e92802421818708972678860
wonyong.yun <ywyi1992@gmail.com>
2026-09-09 17:21:22 +0900

    [frontend] lobby hero section에 애니메이션, 반응형 class name 수정 및 custom Link 태그 clickEventName 추가.

--- frontend/src/pages/lobby/landing/components/LandingHeroSection.tsx
+++ frontend/src/pages/lobby/landing/components/LandingHeroSection.tsx
@@ -1,50 +1,56 @@
+import { FC } from "react";
 
 import LogoImage from "@/components/LogoImage";
-import LandingHeroCalendarPreview from "./LandingHeroCalendarPreView";
-import styles from "./LandingHeroSection.module.css";
 import Link from "@/components/link";
+import { CalendarEventMainTypes } from "@/domains/calendar/calendarType";
+
+import LandingHeroCalendarPreview from "./LandingHeroCalendarPreView";
+
+import styles from "./LandingHeroSection.module.css";
+
+import type { CalendarEventMainType } from "@/domains/calendar/calendarType";
 
 const LandingHeroSection = () => {
     return (
-        <section
-            className={ styles.wrap }
-        >
-            <div className={ styles.innerBox }>
-                <div
-                    className={ styles.logoBox }
-                >
+        <section className={ styles.hero }>
+            <div className={ styles.heroContent }>
+                <div className={ styles.heroLogo }>
                     <LogoImage
-                        className={ styles.logoImage }
+                        className={ styles.heroLogoImage }
                     />
-                    <span className={ styles.brandBox }>
-                        <span className={ styles.brandText1 }>
-                            OUR 
+                    <span className={ styles.heroBrand }>
+                        <span className={ styles.heroBrandPrimary }>
+                            OUR
                         </span>
-                        <span className={ styles.brandText2 }>
+                        <span className={ styles.heroBrandSecondary }>
                             CALENDAR
                         </span>
                     </span>
                 </div>
-                <div
-                    className={ styles.sub }
-                >
-                    <p className={ styles.subTitle }>
-                        우리들의 계획적인 하루를 위해! 
+                <div className={ styles.heroText }>
+                    <p className={ styles.heroTitle }>
+                        우리들의 계획적인 하루를 위해!
                     </p>
-                    <p className={styles.subDescription}>
-                        일정부터 미션, 가계부, 인증까지
+                    <p className={ styles.heroDescription }> 
+                        <CalendarDotLegendText
+                            eventType={ CalendarEventMainTypes.MISSION }
+                        />부터 
+                        <CalendarDotLegendText
+                            eventType={ CalendarEventMainTypes.TRANSACTION }
+                        />,
+                        <CalendarDotLegendText
+                            eventType={ CalendarEventMainTypes.SCHEDULE }
+                        />까지
                         <br />
                         우리 그룹의 계획과 기록을 한곳에서 관리하세요.
                     </p>
                 </div>
-
                 <Link
-                    to={ "/login" }
-                    className={ styles.primaryButton }
+                    to="/login"
+                    className={ styles.heroButton }
+                    clickEventName="LOBBY_HERO_BTN"
                 >
-                    <span
-                        className={ styles.primaryButtonText }
-                    >
+                    <span className={ styles.heroButtonText }>
                         아워캘 시작하기
                         <span aria-hidden="true">
                             →
@@ -52,12 +58,29 @@ const LandingHeroSection = () => {
                     </span>
                 </Link>
             </div>
-            <div className={styles.heroVisual}>
+            <div className={ styles.heroVisual }>
                 <LandingHeroCalendarPreview />
-                {/* <CalendarPreview /> */}
             </div>
         </section>
     );
 };
 
+const calendarDotLegendTexts = {
+    [CalendarEventMainTypes.MISSION]: "미션",
+    [CalendarEventMainTypes.SCHEDULE]: " 일정",
+    [CalendarEventMainTypes.TRANSACTION]: " 가게부",
+};
+
+const CalendarDotLegendText: FC<{ eventType: CalendarEventMainType }> = ({ eventType }) => {
+    return (
+        <span
+            className={ styles.dotLegendText }
+            data-event-type={ eventType }
+        >
+            {
+                calendarDotLegendTexts[eventType]
+            }
+        </span>
+    );
+};
 export default LandingHeroSection;
