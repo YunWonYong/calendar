8c7182d6b67f94a802bc562097182ac1edb28828
wonyong.yun <ywyi1992@gmail.com>
2026-09-10 17:38:08 +0900

    [frontend] auto login overlay 추가.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,31 @@
 import { RouterProvider } from "react-router-dom";
 
 import router from "@/router";
+
 import ThemeProvider from "./contexts/theme/ThemeProvider";
+import AuthProvider from "./contexts/auth/AuthProvider";
+import AutoLoginOverlay from "./components/auth/AutoLoginOverlay";
+
+import useAuth from "./hooks/auth/useAuth";
 
 import "./styles/theme.css";
+
+const AutoLoginOverlayWrap = () => {
+    const { isLoading } = useAuth();
+    if (isLoading) {
+        return <AutoLoginOverlay />
+    }
+};
 
 const App = () => {
     return (
         <ThemeProvider>
-            <RouterProvider 
-                router={ router }
-            />
+            <AuthProvider >
+                <AutoLoginOverlayWrap />
+                <RouterProvider 
+                    router={ router }
+                />
+            </AuthProvider>
         </ThemeProvider>
     );
 };
